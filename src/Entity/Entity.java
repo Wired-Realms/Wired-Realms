1d49b2fbc66efa93ece0c9fa4b2ed008af615317
Alin Andrei Abahnencei <alinandrei2004@gmail.com>
2024-12-20 21:56:00 +0200

    [feat]: Add player animations
    Signed-off-by: Alin Andrei Abahnencei <alinandrei2004@gmail.com>

--- src/Entity/Entity.java
+++ src/Entity/Entity.java
@@ -7,6 +7,9 @@ public class Entity {
     public int x, y;
     public int speed;
 
-    public BufferedImage defaultPlayer;
+    public BufferedImage defaultPlayer, up1, up2, down1, down2, left1, left2, right1, right2;
     public String direction;
+
+    public int spriteCounter = 0;
+    public int spriteNum = 1;
 }
--- src/Entity/Player.java
+++ src/Entity/Player.java
@@ -26,7 +26,7 @@ public class Player extends Entity{
         x = 100;
         y = 100;
         speed = 4;
-        direction = "down";
+        direction = "none";
     }
 
     public void getPlayerImage() {
@@ -35,6 +35,22 @@ public class Player extends Entity{
 
             defaultPlayer = ImageIO.read(Objects.requireNonNull(getClass().getClassLoader().
                     getResource("player/player.png")));
+            up1 = ImageIO.read(Objects.requireNonNull(getClass().getClassLoader().
+                    getResource("player/PlayerUp1.png")));
+            up2 = ImageIO.read(Objects.requireNonNull(getClass().getClassLoader().
+                    getResource("player/PlayerUp2.png")));
+            down1 = ImageIO.read(Objects.requireNonNull(getClass().getClassLoader().
+                    getResource("player/PlayerDown1.png")));
+            down2 = ImageIO.read(Objects.requireNonNull(getClass().getClassLoader().
+                    getResource("player/PlayerDown2.png")));
+            left1 = ImageIO.read(Objects.requireNonNull(getClass().getClassLoader().
+                    getResource("player/PlayerLeft1.png")));
+            left2 = ImageIO.read(Objects.requireNonNull(getClass().getClassLoader().
+                    getResource("player/PlayerLeft2.png")));
+            right1 = ImageIO.read(Objects.requireNonNull(getClass().getClassLoader().
+                    getResource("player/PlayerRight1.png")));
+            right2 = ImageIO.read(Objects.requireNonNull(getClass().getClassLoader().
+                    getResource("player/PlayerRight2.png")));
 
         } catch (IOException e) {
             e.printStackTrace();
@@ -42,49 +58,98 @@ public class Player extends Entity{
     }
 
     public void update() {
-        if (keyH.upPressed && keyH.leftPressed) {
 
-            y -= (speed / Math.sqrt(2));
-            x -= (speed / Math.sqrt(2));
-        } else if (keyH.upPressed && keyH.rightPressed) {
+        if (keyH.leftPressed || keyH.rightPressed || keyH.upPressed || keyH.downPressed) {
 
-            y -= (speed / Math.sqrt(2));
-            x += (speed / Math.sqrt(2));
-        } else if (keyH.downPressed && keyH.leftPressed) {
+            double diagonalSpeed = speed / Math.sqrt(2);
+            if (keyH.upPressed && keyH.leftPressed) {
+                direction = "left";
+                y -= diagonalSpeed;
+                x -= diagonalSpeed;
+            } else if (keyH.upPressed && keyH.rightPressed) {
+                direction = "right";
+                y -= diagonalSpeed;
+                x += diagonalSpeed;
+            } else if (keyH.downPressed && keyH.leftPressed) {
+                direction = "left";
+                y += diagonalSpeed;
+                x -= diagonalSpeed;
+            } else if (keyH.downPressed && keyH.rightPressed) {
+                direction = "right";
+                y += diagonalSpeed;
+                x += diagonalSpeed;
+            } else if (keyH.upPressed) {
+                direction = "up";
+                y -= speed;
+            } else if (keyH.downPressed) {
+                direction = "down";
+                y += speed;
+            } else if (keyH.leftPressed) {
+                direction = "left";
+                x -= speed;
+            } else if (keyH.rightPressed) {
+                direction = "right";
+                x += speed;
+            }
 
-            y += (speed / Math.sqrt(2));
-            x -= (speed / Math.sqrt(2));
-        } else if (keyH.downPressed && keyH.rightPressed) {
-
-            y += (speed / Math.sqrt(2));
-            x += (speed / Math.sqrt(2));
-        } else if (keyH.upPressed) {
-            direction = "up";
-            y -= speed;
-        } else if (keyH.downPressed) {
-            direction = "down";
-            y += speed;
-        } else if (keyH.leftPressed) {
-            direction = "left";
-            x -= speed;
-        } else if (keyH.rightPressed) {
-            direction = "right";
-            x += speed;
+            spriteCounter++;
+            if (spriteCounter > 12) {
+                if (spriteNum == 1) {
+                    spriteNum = 2;
+                } else if (spriteNum == 2) {
+                    spriteNum = 1;
+                }
+                spriteCounter = 0;
+            }
         }
+//        else if (!keyH.downPressed && !keyH.upPressed && !keyH.leftPressed && !keyH.rightPressed) {
+//            direction = "none";
+//        }
     }
 
     public void draw(Graphics2D g2) {
 
         BufferedImage image = null;
 
+//        image = defaultPlayer;
         switch (direction) {
             case "down":
+                if (spriteNum == 1) {
+                    image = down1;
+                }
+                if (spriteNum == 2) {
+                    image = down2;
+                }
+                break;
+            case "up":
+                if (spriteNum == 1) {
+                    image = up1;
+                }
+                if (spriteNum == 2) {
+                    image = up2;
+                }
+                break;
+            case "left":
+                if (spriteNum == 1) {
+                    image = left1;
+                }
+                if (spriteNum == 2) {
+                    image = left2;
+                }
+                break;
+            case "right":
+                if (spriteNum == 1) {
+                    image = right1;
+                }
+                if (spriteNum == 2) {
+                    image = right2;
+                }
+                break;
+            case "none":
                 image = defaultPlayer;
                 break;
         }
 
         g2.drawImage(image, x, y, gamePanel.getTileSize(), gamePanel.getTileSize(), null);
-//        g2.setColor(Color.WHITE);
-//        g2.fillRect(x, y, gamePanel.getTileSize(), gamePanel.getTileSize());
     }
 }
